4c6d01a5ed24e1d8e21fe2bef12d5b7c94fa61bf
Shane Whitlock <shane.whitlock@optilogic.com>
2023-03-04 23:41:34 -0500

    test

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/optiapp-module-runner/",
-  publicDir: 'public',
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true
-  },
   plugins: [react()],
-  resolve: {
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx'],
-  },
 })
